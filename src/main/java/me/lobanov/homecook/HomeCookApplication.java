deac4d4f189db330f527ef51f07882b7c21cf4fc
Alexandr <kitalex777@mail.ru>
2022-12-29 19:31:35 +0300

    initial commit

---
+++ src/main/java/me/lobanov/homecook/HomeCookApplication.java
@@ -0,0 +1,13 @@
+package me.lobanov.homecook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeCookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeCookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/lobanov/homecook/HomeCookApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lobanov.homecook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeCookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
